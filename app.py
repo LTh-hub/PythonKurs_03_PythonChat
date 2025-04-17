2441925c91e1a9ce45b64aaa91bc0f44739979d2
Lars Thomsson <lartho63@gmail.com>
2025-04-17 13:41:15 +0200

    Hela programmet från 3e kurstillfället

--- app.py
+++ app.py
@@ -9,7 +9,6 @@
 import tkinter as tk
 import pymongo
 
-
 # Connect to MongoDB
 client = pymongo.MongoClient("mongodb://localhost:27017")
 
@@ -26,27 +25,27 @@ entry = tk.Entry(root, width=40)
 entry.pack(pady=5)
 
 
-
-
 def send_message():
     if entry.get():
         messages_col.insert_one({"text": entry.get()})
         entry.delete(0, tk.END)
 
 
-
 send_button = tk.Button(root, text="Send", command=send_message)
 send_button.pack(pady=5)
 
 
+message_label = tk.Label(root, text="Message: ", justify="left")
+message_label.pack()
 
 
+def fetch_messages():
+    messages = messages_col.find().sort("_id")
+    message_label.config(text="Messages:\n" + "\n".join(f"- {m['text']}" for m in messages))
+    root.after(2000, fetch_messages)
 
 
-
-
-
-#fetch_messages()
+fetch_messages()
 root.mainloop()
 
 
@@ -57,5 +56,4 @@ root.mainloop()
 # git add .
 # git commit  -m"Added new code for mongo"
 # git push
-
 
