86c43de5c1c2a5f0267d910af0aebd5883b8df8b
Lars Thomsson <lartho63@gmail.com>
2025-04-18 18:08:27 +0200

    Ytterlgare småfix

--- app.py
+++ app.py
@@ -15,7 +15,6 @@ client = pymongo.MongoClient("mongodb://localhost:27017")
 db = client["PythonChat"]
 messages_col = db["messages"]
 
-
 # GUI
 root = tk.Tk()
 root.title("PythonChat")
@@ -24,25 +23,21 @@ root.geometry("300x300")
 entry = tk.Entry(root, width=40)
 entry.pack(pady=5)
 
-
 def send_message():
     if entry.get():
         messages_col.insert_one({"text": entry.get()})
         entry.delete(0, tk.END)
 
-
 send_button = tk.Button(root, text="Send", command=send_message)
 send_button.pack(pady=5)
-
 
 message_label = tk.Label(root, text="Message: ", justify="left")
 message_label.pack()
 
-
 def fetch_messages():
     messages = messages_col.find().sort("_id")
     message_label.config(text="Messages:\n" + "\n".join(f"- {m['text']}" for m in messages))
-    root.after(2000, fetch_messages)
+    root.after(2000, fetch_messages)        # Var annan sekund mot MongoDB (2000)
 
 
 fetch_messages()
@@ -50,10 +45,7 @@ root.mainloop()
 
 
 
-
-
 # git status (frivilligt)
 # git add .
 # git commit  -m"Added new code for mongo"
 # git push
-
